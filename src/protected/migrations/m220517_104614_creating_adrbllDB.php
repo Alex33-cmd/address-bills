ae411456fdfc5bca60e5886fe5ea402072d21513
Alex33-cmd <alaster586@gmail.com>
2022-05-27 12:47:54 +0300

    добавлены тестовые данные для таблиц (прямо в миграцию)

--- src/protected/migrations/m220517_104614_creating_adrbllDB.php
+++ src/protected/migrations/m220517_104614_creating_adrbllDB.php
@@ -22,6 +22,42 @@ class m220517_104614_creating_adrbllDB extends CDbMigration
         ),  'ENGINE=InnoDB CHARSET=utf8'); 
 
         $this->addForeignKey('fk1', 'bill', 'foreignID', 'address', 'id','CASCADE','CASCADE');
+
+        // test data
+        $this->insert('address', array(
+            'gorod' => 'Санкт-Петербург',
+            'adres' => 'Лиговский пр.',
+            'dom' => '10',
+            'korpus' => '2Б',
+            'kvartira' => '104'
+	    ));	
+        $this->insert('address', array(
+            'gorod' => 'Пушкин',
+            'adres' => 'ул. Кима',
+            'dom' => '7',
+            'korpus' => '1',
+            'kvartira' => '39'
+	    ));	
+        $this->insert('bill', array(
+            'foreignID' => '1',
+            'nomer' => '878431',
+            'usluga' => 'Электроэнергия',
+	    ));	
+        $this->insert('bill', array(
+            'foreignID' => '1',
+            'nomer' => '878443',
+            'usluga' => 'Содержание общедомового имущества',
+	    ));	
+        $this->insert('bill', array(
+            'foreignID' => '2',
+            'nomer' => '784613',
+            'usluga' => 'Электроэнергия',
+	    ));		
+        $this->insert('bill', array(
+            'foreignID' => '2',
+            'nomer' => '784417',
+            'usluga' => 'Уборка лестничных площадок',
+	    ));	
     }
  
     public function down()
@@ -30,7 +66,6 @@ class m220517_104614_creating_adrbllDB extends CDbMigration
         $this->dropTable('address');
     }
 
-	/*
 	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
@@ -39,5 +74,4 @@ class m220517_104614_creating_adrbllDB extends CDbMigration
 	public function safeDown()
 	{
 	}
-	*/
 }
